33cdd6ab184761e005acfa47a72f03c1f0cac011
madhank93 <maddymadhan946@gmail.com>
2020-09-06 19:02:15 +0530

    data types and variable

---
+++ README.md
@@ -0,0 +1,15 @@
+# Javascript
+
+## Resources
+
+* [Javascript Info](https://javascript.info/)
+
+* [FreeCodeCamp Javascript](https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org)
+
+* [FreeCodeCamp Javascript](https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org)
+
+* [You Don't know JS](https://github.com/getify/You-Dont-Know-JS)
+
+* [30 sec of code](https://github.com/30-seconds/30-seconds-of-code)
+
+* [Airbnb Javascript style guide](https://github.com/airbnb/javascript)
---
+++ language-basics/data-types-and-variables/data_types.js
@@ -0,0 +1,73 @@
+/*
+
+Data types:
+Undefined, null, boolean, string, symbol, number, bigint and Object.
+
+Variable:
+var, let (preferred way) and const
+
+*/
+
+const foo = 1;
+let bar = foo;
+let firstName = "Madhan";
+let lastName; // undefined  
+
+bar = 9;
+
+console.log(foo, bar,firstName, lastName);
+
+/*
+Variable naming
+    There are two limitations on variable names in JavaScript:
+        * The name must contain only letters, digits, or the symbols $ and _.
+        * The first character must not be a digit.
+*/
+
+let myVeryLongName;
+let test123;
+const PI = 3.14;
+
+// Numbers
+let n = 123;
+n = 12.345;
+
+// BigInt
+// the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+// String (are represented in Double quotes, Single quotes, Backticks)
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`; // String interpolation
+
+// Boolean
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+let isGreater = 4 > 1;
+
+// "null" value
+let age = null;
+
+// "undefined" value (The meaning of undefined is “value is not assigned”)
+let mobileNumber;
+
+//object for more complex data structures.
+
+//symbol for unique identifiers.
+
+// typeof operator
+let num = 0;
+let fname = "Madhan";
+
+console.log(typeof(num), typeof(fname));
+
+
+
+
+
+
+
+
+
+
